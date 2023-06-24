b853b74da641d49b13de2eb342f8a34171f71cf3
Arpit Johri <johri.arpit@gmail.com>
2023-06-24 18:25:22 +0530

    firebase login logic added

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:quzzer/shared/bottom_nav.dart';
+import 'package:quzzer/login/login.dart';
+import 'package:quzzer/topics/topics.dart';
+import 'package:quzzer/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavBar(),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () => Navigator.pushNamed(context, '/about'),
-            child: Text(
-              "About",
-              style: Theme.of(context).textTheme.button,
-            )),
-      ),
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("loading");
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text("error"),
+          );
+        } else if (snapshot.hasData) {
+          return const TopicsScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
